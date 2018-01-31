a019ac9ef6e319c9405197722d570df4e45c23c1
stanlee321 <stanlee321@gmail.com>
2018-01-30 23:15:01 -0400

    added cars_cropper, removed unused files and folders

--- get_plates.py
+++ get_plates.py
@@ -14,24 +14,47 @@ import shutil
 
 class PlateRecognition():
 	def __init__(self, folder='red'):
-		self.db = os.getenv('HOME') + '/WORKDIR'+ '/' + folder +'/' +'rename.db'
-		self.conn = sqlite3.connect(self.db)
-		self.c =  self.conn.cursor()
-		self.c.execute('CREATE TABLE IF NOT EXISTS rename_table(dtime TEXT, folder TEXT, image TEXT, plate TEXT, prob TEXT)')
-		
-	def dynamic_data_entry(self, dtime, folder, image, plate, prob):
 
-		conn = self.conn
-		c = self.c
+		self.folder = folder
+		
+	@staticmethod
+	def create_db(db_name):
+		conn = sqlite3.connect(db_name)
+		c =  conn.cursor()
+		c.execute('CREATE TABLE IF NOT EXISTS rename_table(dtime TEXT, folder TEXT, image TEXT, plate TEXT, prob TEXT)')
+
+	@staticmethod
+	def dynamic_data_entry(db_name, dtime, folder, image, plate, prob):
 
 		DTIME = dtime
 		FOLDER = folder
 		IMAGE = image
 		PLATE = plate
 		PROB = prob
+
+		conn = sqlite3.connect(db_name)
+		c =  conn.cursor()
 		c.execute("INSERT INTO rename_table(dtime, folder, image, plate, prob) VALUES (?,?,?,?,?)",\
 			(DTIME, FOLDER, IMAGE, PLATE, PROB))
 		conn.commit()
+
+		c.close()
+		conn.close()
+
+	@staticmethod
+	def leer_DB(db_name):
+
+		conn = sqlite3.connect(db_name)
+		c =  conn.cursor()
+		c.execute("SELECT folder, image, plate, prob FROM rename_table")
+		data = c.fetchall()
+		homework = []
+		for row in data:
+			metadata = data
+			homework.append(metadata)
+		c.close()
+		conn.close()
+		return homework
 
 	@staticmethod
 	def get_json_from_api(image):
@@ -53,7 +76,6 @@ class PlateRecognition():
 		cv_img = []
 		path = path_to_folders
 		for img in glob.glob("{}/*.{}".format(path, extention)):
-		    #n = cv2.imread(img)
 		    cv_img.append(img)
 		return cv_img
 
@@ -64,32 +86,24 @@ class PlateRecognition():
 		"""
 		folders_to_clean = []
 		total_folders = []
-		print('Path to clean is: ', path_to_clean)	
-		# List of folders in the <path_to_clean> input
 		files = glob.glob(path_to_clean)
 		for folder in files:
-			# Get the whole names in the directory
 			total_folders.append(folder)
-			# Count for the elements in this folde
 		return total_folders
 
 	@staticmethod
 	def get_plates(result):
 		plates = result['candidates'][0]
 		return plates
-		#print(plates)
 
 	@staticmethod
 	def get_plate_region(result):
 		region = result['coordinates'][0]
 		return region
 
-
 	@staticmethod
-	def write_plate(image, region, plate):
+	def write_plate(image_path_name,image_name, folder_to_save, region, plate):
 		# Save image with plate region labeled
-		path = image.split('/')
-		directory = path[1] + '/'+path[2] + '/'+ path[3] + '/'+ path[4] + '/' + path[5] + '/'
 		font = cv2.FONT_HERSHEY_SIMPLEX
 
 		# CREATE the "detected" region in the image
@@ -102,14 +116,12 @@ class PlateRecognition():
 		textx = region[0]['x']
 		texty = region[0]['y']
 
-		img = cv2.imread(image)
+		img = cv2.imread(image_path_name)
 		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 		img = cv2.rectangle(img,(px0,py0),(px1,py1),(0,255,0),3)
 		img = cv2.putText(img, plate,(textx,int(texty*0.95)), font, 1,(0,255,3),2,cv2.LINE_AA)
-		save_in = '/'+ directory + "{}_rekogn.jpg".format(path[6])
+		save_in = folder_to_save + "{}_rekogn.jpg".format(image_name)
 		cv2.imwrite(save_in, img)
-		#scipy.misc.imsave(directory+'/{}/'.format(idd)+'{}_{}.jpg'.format(idd,date),img)
-		#cv2.imshow(directory+'/{}/'.format(idd)+'{}_{}.jpg'.format(idd,date),img)
 
 	@staticmethod
 	def get_information_of_images(image):
@@ -124,50 +136,26 @@ class PlateRecognition():
 		# does not bother us.
 		result_pandas_df = json_normalize(information_of_the_image_as_json_file, 'results')
 		# Possible plates from the above result in the format i.e :
-		#								{'confidence': 94.38839, 'matches_template': 0, 'plate': '2070GKD'},
-		#							    {'confidence': 81.850777, 'matches_template': 0, 'plate': '207QGKD'},
-
 		if len(result_pandas_df) == 0:
 			return ['The API cant abble to detect any plate in the image']
 		else:
 			possible_plates = PlateRecognition.get_plates(result_pandas_df)
 
 			# Working on the max confidence
-			prob = possible_plates[0]['confidence']
-			plate = possible_plates[0]['plate']			
+			prob 			= possible_plates[0]['confidence']
+			plate 			= possible_plates[0]['plate']			
 			possible_region = PlateRecognition.get_plate_region(result_pandas_df)
-			region = possible_region
-
+			region 			= possible_region
 			return [region, prob, plate, information_of_the_image_as_json_file]
-
-
-	@staticmethod
-	def leer_DB():
-		# Read old metadata
-		# Init DB
-		db = os.getenv('HOME') + '/WORKDIR'+ '/' + 'red'+'/' +'rename.db'
-		conn = sqlite3.connect(db,timeout=1)
-		c =  conn.cursor()
-		#c.execute("SELECT * FROM stufftoPlot WHERE value=3 AND keyword='Python'")
-		#c.execute("SELECT keyword,unix,value FROM stufftoPlot WHERE unix >1515634491")
-		c.execute("SELECT folder, image, plate, prob FROM rename_table")
-		#data = c.fetchone()
-		data = c.fetchall()
-
-		homework = []
-		for row in data:
-			metadata = data
-			homework.append(metadata)
-		#print('HOMEWORK', homework)
-
-		c.close()
-		conn.close()
-		return homework
 
 
 	def __call__(self, work_in_folder):
 
-		path_to_folders = os.getenv('HOME') + '/WORKDIR'+ '/' + work_in_folder + '/*'
+		path_to_work_folder = os.getenv('HOME') + '/' + work_in_folder
+		path_to_folders     =  path_to_work_folder + '/*'
+		path_to_db 			= os.getenv('HOME') + '/WORKDIR' + '/' +'rename_{}.db'.format(work_in_folder)
+		# Create DB for this case:
+		PlateRecognition.create_db(path_to_db)
 
 		folders = self.get_folders(path_to_folders)
 		for folder in folders:
@@ -177,43 +165,43 @@ class PlateRecognition():
 				print('Working in image: ', image)
 				information =  PlateRecognition.get_information_of_images(image)
 				if len(information)>1:
-					region, prob, plate, full_data = information[0], information[1], information[2], information[3] 
-					#print(region, prob, plate, full_data)
-					print('IMAGE BEFORE IS....____----->>>>', image)
-					PlateRecognition.write_plate(image, region, plate)
-					print('saving the max PROB. confidence to txt log ...')
-					# writing log...
 					path = image.split('/')
-					directory = '/'+ path[1] + '/' + path[2] + '/'+ path[3] + '/'+ path[4] + '/' + path[5] + '/'
-					np.save(directory + '{}.npy'.format(path[6][0:-4]), full_data)
-					
-					folder = directory
-					image = path[6][0:-4]
-					dtime = path[6][0:-6]
-					self.dynamic_data_entry(dtime, folder, image, plate, prob)
-					#with open(directory +'{}.txt'.format(path[6][0:-4]),'a') as f:
-					#	line = '{} {} {}'.format(plate, prob, region)
-					#	f.write(line)
 
+					old_folder_path = '/'.join(path[0:5]) + '/'
+					old_folder_name = path[3]
+					dtime           = path[4]
+					image_name      = path[-1]
+					image_path_name =  '/'.join(path)
+
+					# get information from Json
+					region, prob, plate, full_data = information[0], information[1], information[2], information[3] 
+					
+					# write the image with this info
+					PlateRecognition.write_plate(image_path_name, image_name, old_folder_path, region, plate)
+					
+					# record the full log from JSON in a npy
+					np.save(old_folder_path + '{}.npy'.format(image_name), full_data)
+					
+					# Commit information to DB
+					PlateRecognition.dynamic_data_entry(path_to_db, dtime, old_folder_path, image_name, plate, prob)
 				else:
 					print(information)
 				# Close coneccions
-
-
+		print('STARING RENANaming...')
+		db_info = PlateRecognition.leer_DB(path_to_db)
+		for row in db_info[0]:
+			old_folder = row[0][0:-1]
+			new_folder = row[0][0:-1] + '_{}_{}'.format(row[2], row[3])
+			try:
+				os.rename(old_folder, new_folder)
+			except:
+				print('This dude dont exist', row[0])
 
 if __name__ == '__main__':
 	folder = 'red'
-	demo = "/home/stanlee321/WORKDIR/red/2018-01-17_07-35-55/2018-01-17_07-35-55_0wm.jpg"
 	platereko = PlateRecognition()
-	#platereko(folder)
-	db = platereko.leer_DB()
-	for row in db[0]:
-		old_folder = row[0][0:-1]
-		new_folder = row[0][0:-1] + '_{}_{}'.format(row[2], row[3])
-		try:
-			os.rename(old_folder, new_folder)
-		except:
-			print('This dude dont exist')
+	platereko(folder)
+
 
 	#platereko.get_json_from_api(demo)
 	#information = platereko.get_information_of_images(demo)
--- sync_to_API/README.md
+++
@@ -1,2 +0,0 @@
-# sync_to_API
-Scripts to sync and process images into respective folders.
